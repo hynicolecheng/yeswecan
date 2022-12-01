a959b60a64c118f8fc9da23800f81127465a4563
Savit Bhat <bhatsavit@berkeley.edu>
2022-11-30 23:36:37 -0800

    updated models to string matching

--- recipefinder/fridgelookup/models.py
+++ recipefinder/fridgelookup/models.py
@@ -6,36 +6,51 @@ class Recipe(models.Model):
         return self.recipe_name
     recipe_name = models.CharField(max_length=200)
     recipe_URL = models.CharField(max_length=400)
+
+
+    ingredients = set() #ingredients.add("egg"), or ingredients.update("egg") works
+
+
     #recipe_ingredient_list = QuerySet()
     #recipe.ingredient_set
 
 class Fridge(models.Model):
     def __str__(self):
         return self.fridge_name
+    
+    ingredients = set() #ingredients.add("egg"), or ingredients.update("egg") works
+
    #fridge_ingredient_list = QuerySet()
     fridge_name = models.CharField(max_length=200)
    #fridge.ingredient_set
     def get_available_recipes(self):
         rv = []
-        #create empty list
-        #iterate through Recipe.objects.all()
         for r in Recipe.objects.all():
-            print(r.ingredient_set)
-            #if len(r.ingredient_set) == len((r.ingredient_set).intersection(self.ingredient_set)):
-            if len(r.ingredient_set.all()) == len((r.ingredient_set.all()).intersection(self.ingredient_set.all())):
+            if len(r.ingredients) == len((r.ingredients).intersection(self.ingredients)):
                 rv.append(r.recipe_URL)
-        #within each iteration, check if the intersection of recipe's ingredients and fridge's ingredients are of equal size. if so, add to initial list.
-        #return list.
+
         return rv
 
+        #rv = []
+        #create empty list
+        #iterate through Recipe.objects.all()
+        #for r in Recipe.objects.all():
+        #    print(r.ingredient_set)
+            #if len(r.ingredient_set) == len((r.ingredient_set).intersection(self.ingredient_set)):
+        #    if len(r.ingredient_set.all()) == len((r.ingredient_set.all()).intersection(self.ingredient_set.all())):
+        #        rv.append(r.recipe_URL)
+        #within each iteration, check if the intersection of recipe's ingredients and fridge's ingredients are of equal size. if so, add to initial list.
+        #return list.
+        #return rv
 
-class Ingredient(models.Model):
-    def __str__(self):
-        return self.name
-    name = models.CharField(max_length=200)
-    recipes = models.ManyToManyField(Recipe) 
-    fridge = models.ForeignKey(Fridge, on_delete=models.CASCADE, default='')
-    def equals(self, other):
-        return (other.name).equals(self.name)
-    #ingredient.recipe_set
-    #ingredient.fridge
+
+#class Ingredient(models.Model):
+#    def __str__(self):
+#        return self.name
+#    name = models.CharField(max_length=200)
+#    recipes = models.ManyToManyField(Recipe) 
+#    fridge = models.ForeignKey(Fridge, on_delete=models.CASCADE, default='')
+#    def equals(self, other):
+#        return (other.name).equals(self.name)
+#    #ingredient.recipe_set
+#    #ingredient.fridge
